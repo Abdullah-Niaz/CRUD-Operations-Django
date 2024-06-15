b1b02d3c5d0ce05ca1c10c613f6d6a501cc06dbe
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-16 04:10:38 +0500

    Updated deployment settings

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,22 @@
-echo "BUILD START"
+#!/bin/bash
+# Install dependencies
+pip install -r requirements.txt
 
-Python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Migrate the database
+python manage.py migrate
+
+
+
+
+
+
+
+
+# echo "BUILD START"
+
+# Python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
