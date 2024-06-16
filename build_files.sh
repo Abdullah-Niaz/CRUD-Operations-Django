e73ec6e8210991225816a458eb1b5afb68d27564
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-16 13:34:39 +0500

    Fix build configuration for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,28 @@
-# build_files.sh
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Migrate the database
+python3 manage.py migrate
+
+
+
+
+
+
+
+
+
+
+
+# # build_files.sh
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# python3.9 manage.py collectstatic
 
 
 # # Install dependencies
