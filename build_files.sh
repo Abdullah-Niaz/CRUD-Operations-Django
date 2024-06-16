79f9d2b0f60421ce955a23c1b4690b8aca6be1cb
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-16 17:58:36 +0500

    sh updated

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
-python3 -m pip install -r requirements.txt
+python3 -m venv myenv
+source myenv/bin/activate
 
+python3 -m pip install -r requirements.txt
 # Collect static files
 python3 manage.py collectstatic 
 
