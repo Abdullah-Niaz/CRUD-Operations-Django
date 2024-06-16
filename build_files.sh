c47a86a608265a1aba1455a02e11778b5dbfcfd6
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-16 17:52:48 +0500

    sh updated

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
-
-# Create and activate virtual environment
-set -e
-
-# Create and activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-
 python3 -m pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic 
 
-# Migrate the database
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
