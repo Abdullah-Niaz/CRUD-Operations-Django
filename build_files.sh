bbd30aa5ab4c6a0c9d1efa759b4821b922ce067c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-16 15:28:03 +0500

    Fix dependency and virtual environment issues

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Migrate the database
+python manage.py migrate
+
+
+
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
